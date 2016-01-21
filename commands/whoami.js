8ba2250f586ca0eb65247971406bccf99aad2102
Ron Waldon <jokeyrhyme@gmail.com>
2016-01-21 11:32:59 +1100

    BC-4 # implement `blinkm bmp whoami` command

--- commands/whoami.js
+++ commands/whoami.js
@@ -1,6 +1,42 @@
 'use strict';
 
-module.exports = function () {
-  console.error('not implemented');
-  process.exit(1);
+// foreign modules
+
+const pify = require('pify');
+const async = require('async');
+const elegantSpinner = require('elegant-spinner');
+const logUpdate = require('log-update');
+
+// local modules
+
+const whoami = require('../lib/whoami');
+
+// this module
+
+const pasync = pify(require('async'));
+
+function logAuthLookup (auth) {
+  const frame = elegantSpinner();
+  let timer = setInterval(() => {
+    logUpdate(`${auth.origin}: ${frame()}`);
+  }, 100);
+  return whoami.lookupUser({ auth })
+    .then((result) => {
+      logUpdate(`${auth.origin}: ${result.name} <${result.email}>`);
+      clearTimeout(timer);
+    })
+    .catch((err) => {
+      clearTimeout(timer);
+      return Promise.reject(err);
+    });
+}
+
+module.exports = function (input, flags, options) {
+  whoami.getAuthentications()
+    .then((auths) => {
+      if (auths.length) {
+        return pasync.eachSeries(auths, async.asyncify(logAuthLookup));
+      }
+      console.log('no authentication data found');
+    });
 };
