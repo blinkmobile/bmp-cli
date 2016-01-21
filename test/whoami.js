1bf1ace93dafd18a875d81e5b4e0fb0f4d27536a
Ron Waldon <jokeyrhyme@gmail.com>
2016-01-21 15:12:11 +1100

    BC-5 # extract general request logic out

--- test/whoami.js
+++ test/whoami.js
@@ -47,37 +47,37 @@ test('getAuthentications', (t) => {
 
 test('lookupUser, 200', (t) => {
   const ORIGIN = 'https://example.com';
-  const req = (url, cb) => {
+  const reqFn = (url, cb) => {
     t.is(url, `${ORIGIN}/_api/v1/dashboard`);
     cb(null, { statusCode: 200 }, '{}');
   };
   return whoami.lookupUser({
     auth: { origin: ORIGIN, credential: 'abcdef' },
-    req
+    reqFn
   });
 });
 
 test('lookupUser for HTTP scope, 200', (t) => {
   const ORIGIN = 'http://example.com';
-  const req = (url, cb) => {
+  const reqFn = (url, cb) => {
     t.is(url, `https://example.com/_api/v1/dashboard`);
     cb(null, { statusCode: 200 }, '{}');
   };
   return whoami.lookupUser({
     auth: { origin: ORIGIN, credential: 'abcdef' },
-    req
+    reqFn
   });
 });
 
 test('lookupUser, error', (t) => {
   const ORIGIN = 'https://example.com';
-  const req = (url, cb) => {
+  const reqFn = (url, cb) => {
     t.is(url, `${ORIGIN}/_api/v1/dashboard`);
     cb(new Error('blah'));
   };
   return whoami.lookupUser({
     auth: { origin: ORIGIN, credential: 'abcdef' },
-    req
+    reqFn
   })
     .catch((err) => {
       t.is(err.message, 'blah');
@@ -86,13 +86,13 @@ test('lookupUser, error', (t) => {
 
 test('lookupUser, 403', (t) => {
   const ORIGIN = 'https://example.com';
-  const req = (url, cb) => {
+  const reqFn = (url, cb) => {
     t.is(url, `${ORIGIN}/_api/v1/dashboard`);
     cb(null, { statusCode: 403 });
   };
   return whoami.lookupUser({
     auth: { origin: ORIGIN, credential: 'abcdef' },
-    req
+    reqFn
   })
     .catch((err) => {
       t.is(err.message, '403');
