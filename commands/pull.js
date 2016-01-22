b114761aa720b5022909e2e2f8a8ea5b031d0b5e
Ron Waldon <jokeyrhyme@gmail.com>
2016-01-22 15:22:20 +1100

    BC-6 # download interactions to JSON files

--- commands/pull.js
+++ commands/pull.js
@@ -1,6 +1,23 @@
 'use strict';
 
-module.exports = function () {
-  console.error('not implemented');
-  process.exit(1);
+// local modules
+
+const pull = require('../lib/pull');
+const scope = require('../lib/scope');
+const whoami = require('../lib/whoami');
+
+// this module
+
+module.exports = function (input, flags, options) {
+  let projectScope;
+  let auth;
+  scope.read()
+    .then((s) => {
+      projectScope = s;
+      return whoami.getAuthentication({ scope: projectScope });
+    })
+    .then((a) => {
+      auth = a;
+      return pull.pullAnswerSpace({ auth, scope: projectScope });
+    });
 };
